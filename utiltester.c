1173e9341d596b7c79b89b1cbe61bea55b93e3b2
huzaifa <huzaifakashif2003@gmail.com>
2023-12-19 19:24:52 +0500

    1st test case for degree to radians added

--- utiltester.c
+++ utiltester.c
@@ -4,5 +4,19 @@
 int main()
 {
     //adding test casses for sarey functions.
+    double a=45.0;
+    printf("Testing degrees to radians:");
+    printf("\nInput:%f degrees\n",a);
+    printf("expected output:%f radians\n",degreesToRadians(a));
+    printf("Actual output:%f radians\n",degreesToRadians(a));
+    if(degreesToRadians(a)== 0.785)
+    {
+        printf("test passed!!!! wohoooo");
+    }
+    else{
+        printf("test failed!!!! Awwwww");
+    }
+    
+
 }
 
