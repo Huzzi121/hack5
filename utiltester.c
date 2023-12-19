03d5fb4ff27a7b3f6cd0d1baf2a8f73a6112e512
huzaifa <huzaifakashif2003@gmail.com>
2023-12-19 19:57:35 +0500

    lorentz time dilation test case added

--- utiltester.c
+++ utiltester.c
@@ -21,7 +21,7 @@ int main()
 
     double lat1=37.7749,lon1=-122.4194,lat2=34.0522,lon2=-118.2437;//values were taken from google.hehe
 
-        printf("testing Air Distance");
+        printf("testing Air Distance:");
 
     printf("input:(%f,%f) to (%f,%f)\n",lat1, lon1, lat2, lon2);
     printf("Expected Output: %f km\n", getAirDistance(lat1, lon1, lat2, lon2));
@@ -35,5 +35,24 @@ int main()
         printf("test failed!!!!!Awwwwwwwwww");
     }
 
+
+    //adding test case for lorentztimedilation.
+
+    double t1=1.0,s1=10.0;
+    printf("testing lorentztimedilation:");
+    printf("Input: t = %f, percentC = %f\n", t1, s1);
+    printf("Expected Output: %f\n", lorentzTimeDilation(t1, s1));
+    printf("Actual Output: %f\n", lorentzTimeDilation(t1, s1));
+
+    if(lorentzTimeDilation(t1,s1)==1.0000000007)//value calculated by myself
+    {
+        printf("test passed !!!!!!!!!wohooo");
+    }
+    else 
+    {
+        printf("test failed!!!!! Awwwwww");
+    }
+
+    return 0;
 }
 
